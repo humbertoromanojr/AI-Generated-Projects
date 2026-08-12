827745631c4ece933d74d0c3daaafe36263a0b7f
humbertoromanojr <astronomi@gmail.com>
2026-08-12 10:20:22 -0300

    API ERROR fixed

--- fluttmov/lib/src/core/config/api_config.dart
+++ fluttmov/lib/src/core/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   const ApiConfig._();
 
-  static const String baseUrl = 'https://api.themoviedb.org/3/';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p';
   static const String language = 'pt-BR';
 
